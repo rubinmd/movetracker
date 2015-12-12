81aeb815ba439984b0bacd4c1ecbde80acb1611d
rubinmd <matt@Matthews-MacBook-Pro.local>
2015-12-11 18:49:35 -0600

    one more

--- db/migrate/20151211185958_really_all_fks.rb
+++
@@ -1,7 +0,0 @@
-class ReallyAllFks < ActiveRecord::Migration
-  def change
-    change_column :moves, :user_id, :integer
-    change_column :stale_listings, :move_id, :integer
-    change_column :stale_listings, :contact_id, :integer
-  end
-end
